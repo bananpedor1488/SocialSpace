e39034d9c782e60be488b7898b3fcbe4a9164cee
bananpedor1488 <bananpedor1488@gmail.com>
2025-07-23 12:36:04 +0300

    ы

--- server/models/Post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PostSchema = new mongoose.Schema({
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  content: { type: String, required: true },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-}, { timestamps: true });
-
-module.exports = mongoose.model('Post', PostSchema);
